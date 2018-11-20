a75d3e227b6cb5d7348ec2d277068339b9e9bf7e
Thomas Wilson <twilson.spqr@gmail.com>
2018-11-20 13:51:57 -0500

    spit and polish

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'carrierwave',             '1.2.2'
 gem 'mini_magick',             '4.7.0'
 gem 'font-awesome-rails'
 gem 'devise',                  '~> 4.3.0'
+gem 'omniauth'
 gem 'omniauth-google-oauth2',  '~> 0.2.2'
 gem 'dotenv-rails'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_omniauth(auth)
-    # Either create a User record or update it based on the provider (Google) and the UID
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.token = auth.credentials.token
-      user.expires = auth.credentials.expires
-      user.expires_at = auth.credentials.expires_at
-      user.refresh_token = auth.credentials.refresh_token
-    end
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+      data = access_token.info
+      user = User.where(provider: access_token.provider, uid: access_token.uid ).first
+      if user
+        return user
+      else
+        registered_user = User.where(email: access_token.info.email).first
+        if registered_user
+          return registered_user
+        else
+          user = User.create(name: data["name"],
+            provider:access_token.provider,
+            email: data["email"],
+            uid: access_token.uid ,
+            password: Devise.friendly_token[0,20],
+          )
+        end
+     end
   end
+
 
   def logged_in?
     !current_user.nil?
