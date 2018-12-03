acb967ae37cdfb25873d8f3364b311652e0afabb
Thomas Wilson <twilson.spqr@gmail.com>
2018-12-03 11:36:08 -0500

    fixed weird new devise issue in spec

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20181203155523) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "last_punch", default: "2018-12-03 00:48:39"
+    t.datetime "last_punch", default: "2018-12-03 16:33:52"
     t.index ["user_id", "created_at"], name: "index_projects_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
