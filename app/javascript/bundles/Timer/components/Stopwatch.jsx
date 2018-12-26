2e5ed7e3ed038fbdbb546506e0584622f131090d
Thomas Wilson <twilson.spqr@gmail.com>
2018-12-25 20:09:23 -0500

    adjust timer on load

--- app/javascript/bundles/Timer/components/Stopwatch.jsx
+++ app/javascript/bundles/Timer/components/Stopwatch.jsx
@@ -3,31 +3,33 @@ import React, {Component} from 'react';
 
 class Stopwatch extends Component {
   state = {
-    runningTime: 0,
     startTime: this.props.startTime,
-    prettyTime: "00:00:00"
+    prettyTime: this.formatTime(Date.now() -this.props.startTime)
   };
+  formatTime(ms) {
+    let hours = Math.floor((ms/1000/60/60) << 0).toString();
+    let minutes = Math.floor((ms/1000/60) % 60).toString();
+    let seconds = Math.floor((ms/1000) % 60).toString();
+
+    if (hours.length <2) {
+      hours = "0" + hours;
+    };
+
+    if (minutes.length <2) {
+      minutes = "0" + minutes;
+    };
+
+    if (seconds.length <2) {
+      seconds = "0" + seconds;
+    };
+
+    return hours + ":" + minutes + ":" + seconds;
+  }
   componentDidMount() {
     this.setState(state => {
       this.timer = setInterval(() => {
-        let ms = Date.now() - this.props.startTime
-        let hours = Math.floor((ms/1000/60/60) << 0).toString();
-        let minutes = Math.floor((ms/1000/60) % 60).toString();
-        let seconds = Math.floor((ms/1000) % 60).toString();
-
-        if (hours.length <2) {
-          hours = "0" + hours;
-        };
-
-        if (minutes.length <2) {
-          minutes = "0" + minutes;
-        };
-
-        if (seconds.length <2) {
-          seconds = "0" + seconds;
-        };
-
-        let newPrettyTime = hours + ":" + minutes + ":" + seconds;
+        let ms = Date.now() - this.state.startTime
+        let newPrettyTime = this.formatTime(ms);
         this.setState({
           prettyTime: newPrettyTime
         });
@@ -35,7 +37,7 @@ class Stopwatch extends Component {
     });
   };
   render() {
-    const { runningTime, prettyTime } = this.state;
+    const { prettyTime } = this.state;
     return (
       <div className="col-12 clock-screen">
         <span>{prettyTime}</span>
